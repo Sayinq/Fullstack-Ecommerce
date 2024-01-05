f517e99c9eb56be2654cc9b6c7f1ad4012c27537
MylesGit <37464485+Sayinq@users.noreply.github.com>
2024-01-04 20:13:23 -0500

    initial commit to set foundation for backend and front-end separation

---
+++ pc-fullstack/frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <h1 className="text-4xl font-black text-black">THIS IS TEXT ON A SCREEN</h1>
+    </main>
+  )
+}
